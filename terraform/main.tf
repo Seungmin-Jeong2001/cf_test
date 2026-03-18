450f76c75ae46f97398d6f34574381686b81e98a
catbro-Jeong <108501342+Seungmin-Jeong2001@users.noreply.github.com>
2026-03-18 15:08:40 +0900

    Update Terraform configuration for AWS and Cloudflare

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,48 @@
-# ... (기존 설정 동일) ...
+terraform {
+  required_providers {
+    aws        = { source = "hashicorp/aws", version = "~> 5.0" }
+    cloudflare = { source = "cloudflare/cloudflare", version = "~> 4.0" }
+  }
+}
 
-# 3. Cloudflare 터널 생성
+provider "aws" { region = "ap-northeast-2" }
+provider "cloudflare" { api_token = var.cf_api_token }
+
+# ---------------------------------------------------------
+# 1. AWS Infrastructure (EC2 & Security Group)
+# ---------------------------------------------------------
+resource "aws_instance" "k3s_server" {
+  ami                    = "ami-0ea4d4b8dc1e46212" # Ubuntu 22.04
+  instance_type          = "t3.small"
+  key_name               = "chilseongpa_keypair"
+  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
+  tags                   = { Name = "k3s-server" }
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+}
+
+resource "aws_security_group" "k3s_sg" {
+  name = "k3s-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ---------------------------------------------------------
+# 2. Cloudflare Zero Trust Tunnel
+# ---------------------------------------------------------
 resource "cloudflare_zero_trust_tunnel_cloudflared" "k8s_tunnel" {
   account_id = var.cf_account_id
   name       = "chilseong-tunnel"
@@ -9,8 +51,7 @@ resource "cloudflare_zero_trust_tunnel_cloudflared" "k8s_tunnel" {
 
 resource "random_string" "tunnel_secret" { length = 32 }
 
-# [추가] 터널 설정 (Ingress Rule): 
-# 대시보드의 'Public Hostname' 설정을 코드로 구현한 것입니다.
+# 3. 터널 라우팅 룰 (Public Hostname 설정)
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "aws_tunnel_config" {
   account_id = var.cf_account_id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.id
@@ -26,7 +67,7 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "aws_tunnel_config" {
   }
 }
 
-# 4. DNS 레코드 
+# 4. DNS 레코드 설정
 resource "cloudflare_record" "app_cname" {
   zone_id = var.cf_zone_id
   name    = "app"
@@ -35,11 +76,11 @@ resource "cloudflare_record" "app_cname" {
   proxied = true
 }
 
-# 5. Ansible 인벤토리 자동 생성 (토큰 포함)
+# 5. Ansible 인벤토리 자동 생성
 resource "local_file" "inventory" {
   content  = <<EOT
 [aws]
-${aws_instance.k3s_server.public_ip} ansible_user=ec2-user ansible_private_key_file=${path.cwd}/../chilseongpa_keypair.pem
+${aws_instance.k3s_server.public_ip} ansible_user=ubuntu ansible_private_key_file=/home/jeong/github/cf_test/chilseongpa_keypair.pem
 
 [aws:vars]
 tunnel_token=${cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.tunnel_token}
