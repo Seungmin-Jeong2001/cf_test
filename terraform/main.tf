b8c041625fea130093090d698cae0d2355bbc260
Jeong <mis084110@gmail.com>
2026-03-19 11:16:52 +0900

    최종 해결

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,25 +16,30 @@ provider "google" {
   credentials = var.gcp_credentials
 }
 
-data "cloudflare_zone" "domain" {
-  zone_id = var.cf_zone_id
-}
-
 # -------------------------------------------------------------------
-# AWS Resources (Sub)
+# Cloudflare Load Balancer Monitor (Header 수정 완료)
 # -------------------------------------------------------------------
+resource "cloudflare_load_balancer_monitor" "monitor" {
+  account_id     = var.cf_account_id
+  type           = "http"
+  path           = "/"
+  port           = 80
+  interval       = 60
+  retries        = 2
+  expected_codes = "200"
 
-data "aws_ami" "al2023" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*-x86_64"]
+  # 터널 통과를 위한 필수 헤더 설정
+  header {
+    header = "Host"
+    values = ["app.bucheongoyangijanggun.com"]
   }
 }
 
+# -------------------------------------------------------------------
+# AWS Resources (Ubuntu 24.04 x86_64)
+# -------------------------------------------------------------------
 resource "aws_instance" "k3s_server" {
-  ami                    = "ami-0ea4d4b8dc1e46212" # Ubuntu 22.04
+  ami                    = "ami-084a56dceed3eb9bb" # Ubuntu 24.04 LTS x86_64
   instance_type          = "t3.small"
   key_name               = "chilseongpa_keypair"
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]
@@ -79,28 +84,21 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "aws_config" {
       hostname = "app.bucheongoyangijanggun.com"
       service  = "http://localhost:80"
     }
-    ingress_rule {
-      service = "http_status:404"
-    }
+    ingress_rule { service = "http_status:404" }
   }
 }
 
 # -------------------------------------------------------------------
-# GCP Resources (Main)
+# GCP Resources (Ubuntu 24.04 x86_64)
 # -------------------------------------------------------------------
-
 resource "google_compute_instance" "gcp_server" {
   name         = "k3s-server-gcp"
   machine_type = "e2-medium"
   zone         = var.gcp_zone
 
-  metadata = {
-    ssh-keys = "ubuntu:${file("../gcp_key.pub")}"
-  }
-
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
       size  = 20
     }
   }
@@ -109,19 +107,10 @@ resource "google_compute_instance" "gcp_server" {
     network = "default"
     access_config {}
   }
-
-  tags = ["ssh-server"]
-}
-
-resource "google_compute_firewall" "allow_ssh" {
-  name    = "allow-ssh"
-  network = "default"
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
+  
+  metadata = {
+    ssh-keys = "ubuntu:${file("../gcp_key.pub")}"
   }
-  source_ranges = var.ssh_cidr_blocks
-  target_tags   = ["ssh-server"]
 }
 
 resource "random_string" "tunnel_secret_gcp" { length = 32 }
@@ -141,27 +130,13 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "gcp_config" {
       hostname = "app.bucheongoyangijanggun.com"
       service  = "http://localhost:80"
     }
-    ingress_rule {
-      service = "http_status:404"
-    }
+    ingress_rule { service = "http_status:404" }
   }
 }
 
 # -------------------------------------------------------------------
-# Cloudflare Load Balancer (Tunnel 기반 Failover)
+# Cloudflare Load Balancer Pools & LB
 # -------------------------------------------------------------------
-
-resource "cloudflare_load_balancer_monitor" "monitor" {
-  account_id     = var.cf_account_id
-  type           = "http"
-  path           = "/"
-  port           = 80
-  interval       = 60
-  retries        = 2
-  expected_codes = "200"
-  # hostname 제거, LB Pool origin 주소 기준으로 헬스 체크
-}
-
 resource "cloudflare_load_balancer_pool" "gcp_pool" {
   account_id = var.cf_account_id
   name       = "gcp-main-pool"
@@ -183,26 +158,20 @@ resource "cloudflare_load_balancer_pool" "aws_pool" {
 }
 
 resource "cloudflare_load_balancer" "lb" {
-  zone_id = var.cf_zone_id
-  name    = "app.bucheongoyangijanggun.com"
-
-  # 메인: GCP Tunnel
-  default_pool_ids = [cloudflare_load_balancer_pool.gcp_pool.id]
-
-  # 장애 시: AWS Tunnel
+  zone_id          = var.cf_zone_id
+  name             = "app.bucheongoyangijanggun.com"
+  default_pool_ids = [cloudflare_load_balancer_pool.gcp_pool.id, cloudflare_load_balancer_pool.aws_pool.id]
   fallback_pool_id = cloudflare_load_balancer_pool.aws_pool.id
-
-  proxied = true
+  proxied          = true
 }
 
 # -------------------------------------------------------------------
 # Ansible Inventory
 # -------------------------------------------------------------------
-
 resource "local_file" "inventory" {
   content = <<EOT
 [aws]
-${aws_instance.k3s_server.public_ip} ansible_user=ec2-user ansible_private_key_file=${path.cwd}/../chilseongpa_keypair.pem tunnel_token=${cloudflare_zero_trust_tunnel_cloudflared.aws_tunnel.tunnel_token}
+${aws_instance.k3s_server.public_ip} ansible_user=ubuntu ansible_private_key_file=${path.cwd}/../chilseongpa_keypair.pem tunnel_token=${cloudflare_zero_trust_tunnel_cloudflared.aws_tunnel.tunnel_token}
 
 [gcp]
 ${google_compute_instance.gcp_server.network_interface[0].access_config[0].nat_ip} ansible_user=ubuntu ansible_private_key_file=${path.cwd}/../gcp_key tunnel_token=${cloudflare_zero_trust_tunnel_cloudflared.gcp_tunnel.tunnel_token}
